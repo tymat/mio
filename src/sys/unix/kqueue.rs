3be0d198928265d7c7426cb84bd162ec14f0901f
Anthony Eufemio <tymat@pris.im>
2016-10-18 15:50:32 -0700

    use nix::libc

--- src/sys/unix/kqueue.rs
+++ src/sys/unix/kqueue.rs
@@ -3,7 +3,7 @@ use event::{self, Event};
 use nix::unistd::close;
 use nix::sys::event::{EventFilter, EventFlag, FilterFlag, KEvent, kqueue, kevent, kevent_ts};
 use nix::sys::event::{EV_ADD, EV_CLEAR, EV_DELETE, EV_DISABLE, EV_ENABLE, EV_EOF, EV_ERROR, EV_ONESHOT};
-use libc::{timespec, time_t};
+use nix::libc::{timespec, time_t};
 use std::{cmp, fmt, slice};
 use std::cell::UnsafeCell;
 use std::os::unix::io::RawFd;
